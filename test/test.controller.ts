7a0fc118adbd9e80c1cf27b162895a94f642dbbc
Максим Вековшинин <watchtime24@yandex.ru>
2025-11-23 15:20:42 +0500

    test

---
+++ test/test.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('test')
+export class TestController {
+  @Get()
+  test() {
+    return {
+      message: 'API is working!',
+      timestamp: new Date().toISOString(),
+    };
+  }
+
+  @Get('health')
+  health() {
+    return {
+      status: 'OK',
+      service: 'NestJS Auth Service',
+      timestamp: new Date().toISOString(),
+    };
+  }
+}
